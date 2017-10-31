950cca485b21eb7e721f339da09d3f2982358e79
FERDEDDO <FERDEDDO@GMAIL.com>
2017-10-31 17:49:56 +0100

    Initial commit

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
  def setup
     @user = users(:michael)
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
   
  test "login with valid information followed by logout" do
     get login_path
