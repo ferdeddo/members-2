e5e1784d99f92cf4f94dec08bf1a1df86d4294c5
FERDEDDO <FERDEDDO@GMAIL.com>
2017-10-31 09:18:13 +0100

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,35 @@ class UsersController < ApplicationController
 	
 	def show
 		@user = User.find(params[:id])
+		
 	end
 
 	def home
-		@user = User.new
+		
 	end
   def new
   	@user = User.new
   end
+
+   def secret
+   
+   end
+
+   def create
+  @user = User.new(user_params)
+ 
+  if @user.save
+    redirect_to @user
+  else
+    render 'secret'
+  end
+end
+
+private
+ def user_params
+ 	params.require(:user).permit(:email, :password)
+ end
+   
+
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
+
  root 'users#home'
  get '/new', to: 'users#new'
+ get '/secret', to: 'users#secret'
 
  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
